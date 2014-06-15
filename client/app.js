cc1337b2d8ffa1e6ead7f40b9667dff711ec1ad4
Armando Gonzalez <iam@armando.mx>
2014-06-14 20:35:59 -0500

    se agregaron las plantillas para pagos con tarjeta

---
+++ client/app.js
@@ -0,0 +1,18 @@
+
+/*
+	Eventos de la plantilla de compropago
+ */
+Template.compropago.events({
+  'click #SubmitCompropago': function () {
+    alert(1232);
+  }
+});
+
+/*
+	Eventos de la plantilla de compropago
+ */
+Template.tarjeta.events({
+  'click #SubmitCC': function () {
+    alert(1232);
+  }
+});
